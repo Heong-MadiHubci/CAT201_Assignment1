d31546bf088988bcd65b9274633462ef4feb31d3
Per Ming <perminggwee@gmail.com>
2022-11-14 12:14:39 +0800

    Setup landing page

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-<body>
-
-<h1>My first PHP page</h1>
 
 <?php
 echo "Hello World!";
 ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAT201 Assignment 1</title>
+    <link rel="stylesheet" href="/css/Landing.css">
+</head>
+<body>
+
+<div class="Box">
+    <h1>Landing Page</h1>
+    <p>to convert an Excel file to .csv file using Apache POI library (https://poi.apache.org/download.html).</p>
+</div>
+
+
+
 
 </body>
 </html>
