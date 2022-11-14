816b3cc9360a4edd5b1cba6a04663dd7b4c3867a
Per Ming <perminggwee@gmail.com>
2022-11-14 13:21:36 +0800

    styling landing page

--- index.php
+++ index.php
@@ -2,20 +2,26 @@
 <html lang="en">
 
 <?php
-echo "Hello World!";
+echo "Heong madihubci";
 ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CAT201 Assignment 1</title>
-    <link rel="stylesheet" href="/css/Landing.css">
+    <link rel="stylesheet" href="css/Landing.css">
 </head>
 <body>
 
 <div class="Box">
     <h1>Landing Page</h1>
     <p>to convert an Excel file to .csv file using Apache POI library (https://poi.apache.org/download.html).</p>
+    <div class="button">
+        <a href="index.php">Home</a>
+    </div>
+    <div class="image">
+        <img src="Public/Final Profile Picture.jpg" alt="Testing" >
+    </div>
 </div>
 
 
