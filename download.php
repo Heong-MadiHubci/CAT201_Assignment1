67b81711964be1d0dd6817ce23e78e12ae8dcabd
Per Ming <perminggwee@gmail.com>
2022-12-08 21:43:10 +0800

    Index.php front end

---
+++ download.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href=""  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--CSS-->
+    <link rel="stylesheet" href="css/Main.css">
+    <title>Download CSV converted</title>
+</head>
+
--- index.php
+++ index.php
@@ -1,31 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-echo "Heong madihubci";
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CAT201 Assignment 1 Test</title>
+    <title>CAT201 Assignment Excel to CSV Converter</title>
     <link rel="stylesheet" href="css/Landing.css">
 </head>
-<body>
+    <body>
+        <article>
+            <div class="itemWrapper">
+                <div class="itemBox">
+                    <div class="Title">
+                        <h1>Excel to CSV Converter</h1>
+                        <h3>Convert file from excel to CSV format online for free.</h3>
+                    </div>
 
-<div class="Box">
-    <h1>Landing Page</h1>
-    <p>to convert an Excel file to .csv file using Apache POI library (https://poi.apache.org/download.html).</p>
-    <div class="button">
-        <a href="index.php">Home</a>
-    </div>
-    <div class="image">
-        <img src="Public/Final Profile Picture.jpg" alt="Testing" >
-    </div>
-</div>
+                    <div class="uploadContainer">
+                        <form action = "download.php" method = "POST" enctype="multipart/form-data">
+                            <div class="Card">
+                                <img src="Public/selectFile.png" alt="fileIcon" width="313" height="313">
+                                <input type="file" name="userFile" value="excel" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
+                            </div>
+                            <div class="Submit">
+                                <button type="submit" value="Convert" name="submit">Convert</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="descContainer">
+                        <div class="step1">
+                            <div class="imgBox" >
+                                <img src="Public/fileIcon.png" alt="fileIcon" width="113" height="113">
+                            </div>
+                            <p>Select an excel file from your computer</p>
+                        </div>
+                        <hr>
+                        <div class="step2">
+                            <div class="imgBox">
+                                <img src="Public/convertIcon.png" alt="convertIcon" width="113" height="113">
+                            </div>
 
+                            <p>Our system will automatically help you to convert the filetype to CSV</p>
+                        </div>
+                        <hr>
+                        <div class="step3">
+                            <div class="imgBox">
+                                <img src="Public/downloadIcon.png" alt="downloadIcon" width="113" height="113">
+                            </div>
+                            <p>Download your converted file immediately</p>
+                        </div>
+                    </div>
+                </div>
 
-
-
-</body>
+            </div>
+        </article>
+    </body>
 </html>
